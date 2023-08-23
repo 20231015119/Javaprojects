436a5ba7a49b699fad181eee95600b9dbe4710fd
20231015119 <142996881+20231015119@users.noreply.github.com>
2023-08-23 09:49:11 -0500

    Add files via upload

---
+++ PrimerosProgramas/TreeJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package treejava;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class TreeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey there, I'm Henry and I'm learning to code in java!");
+        System.out.println("Im going to plant a tree today!" + "Ready to get my hands dirty!");
+        System.out.println("    *    ");
+        System.out.println("   ***     ");
+        System.out.println("**********");
+        System.out.println("  *****  ");
+        System.out.println("   ***   ");
+        System.out.println("    *   ");
+        System.out.println("    *   ");
+        System.out.println("    *   ");
+        System.out.println("    *   ");
+        }
+}
