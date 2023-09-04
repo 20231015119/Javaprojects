91ca6570c44c9a5a48717e1bc2a94312e92fe538
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:37:39 -0500

    Add files via upload

---
+++ PrimerosProgramas/Compound_Operators.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compound_operators;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Compound_Operators {
+     public static void main(String[] args) {   
+        int numCookies = 17;
+        numCookies -= 3;
+        System.out.println(numCookies);
+        numCookies /= 2;
+        System.out.println(numCookies);
+    }
+}
