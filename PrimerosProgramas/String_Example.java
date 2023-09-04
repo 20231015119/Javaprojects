4b12b6e13516038dd62e307d1c61b380bad43d58
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:45:28 -0500

    Add files via upload

---
+++ PrimerosProgramas/String_Example.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package string_example;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class String_Example {
+    public static void main(String[] args) {   
+    String openingLyrics="Yesterday, all my troubles seemed so far away";
+    System.out.println(openingLyrics);
+    }
+}
