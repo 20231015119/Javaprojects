2d568b3d03e3cc8171106d4947654f1397f0cdf5
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:33:58 -0500

    Add files via upload

---
+++ PrimerosProgramas/Char_Example.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package char_example;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Char_Example {
+	public static void main(String[] args)  {   
+     char expectedGrade= 'C'; 
+     System.out.println(expectedGrade);
+	}
+}
+    
+    
+
