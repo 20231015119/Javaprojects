6e5c10ecf0397b1ed68ca6211457454a44996732
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:50:01 -0500

    Add files via upload

---
+++ PrimerosProgramas/Variables1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Variables1 {
+public static void main(String[] args) {
+    String name="James Gosling";
+    int yearCreated= 1995;     
+    System.out.println(name);
+    System.out.println(yearCreated);
+	}
+}
