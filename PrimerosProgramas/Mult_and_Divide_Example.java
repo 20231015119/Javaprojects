9161b7206808b1317f84a8cb692f7e269555942b
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:43:47 -0500

    Add files via upload

---
+++ PrimerosProgramas/Mult_and_Divide_Example.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mult_and_divide_example;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Mult_and_Divide_Example {
+    public static void main(String[] args) {   
+        double subtotal= 30;
+    double tax= 0.0875;
+    double total= subtotal+(subtotal*tax);
+    double perPerson= total/4;
+
+    System.out.println(total);
+    System.out.println(perPerson);
+    }
+    
+}
