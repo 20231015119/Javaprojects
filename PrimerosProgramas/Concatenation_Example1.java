a850d565a61bc6f745070fdfb168510ba3fe58f5
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:41:17 -0500

    Add files via upload

---
+++ PrimerosProgramas/Concatenation_Example1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package concatenation_example1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Concatenation_Example1 {
+
+    public static void main(String[] args){
+      int animals= 12;
+      String species= "zebra";
+      String zooDescription= "Our zoo has " + animals + " " + species + "s!"; 
+      System.out.println(zooDescription);
+    }
+    
+}
