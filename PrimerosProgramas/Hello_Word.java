685b131ab598158d54bb4a15920fc5662c53c3f9
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-04 15:48:29 -0500

    Add files via upload

---
+++ PrimerosProgramas/Hello_Word.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello_word;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Hello_Word {
+    public static void main(String args[]){  
+         System.out.println("Hello Java");  
+        }  
+    }  
