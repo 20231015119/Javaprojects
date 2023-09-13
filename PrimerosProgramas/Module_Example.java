ca57bcafaa36b5dea4ded70d9e6457ec19f3af40
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-13 09:35:20 -0500

    Add files via upload

---
+++ PrimerosProgramas/Module_Example.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package module_example;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Module_Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+	int students= 26;
+    int leftOut= students%3; 
+
+    System.out.println(leftOut);
+	}
+}
