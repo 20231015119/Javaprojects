09df6df18938dcae02ad58dc403887d703ed96bb
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-13 09:04:24 -0500

    Add files via upload

---
+++ PrimerosProgramas/Relational_Operators_Example2Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package relational_operators_example2.java;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Relational_Operators_Example2Java {
+        public static void main(String[] args) {   
+		int songsA = 9;
+   int songsB = 9;
+   int albumLengthA = 41;
+   int albumLengthB = 53;
+   boolean sameNumberOfSongs = songsA == songsB;    
+   boolean differentLength = albumLengthA != albumLengthB;
+	}
+}
