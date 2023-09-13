c3ca17e233556c8186bd7948cbdb303502800021
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-13 09:30:23 -0500

    Add files via upload

---
+++ PrimerosProgramas/Operations_OrderJava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operations_order.java;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Operations_OrderJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+
+   int expression1 = 5 % 2 - (4 * 2 - 1);
+    
+
+   int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+  
+
+    int expression3 = 5 * 4 % 3 - 2 + 1;
+    
+	}
+}
