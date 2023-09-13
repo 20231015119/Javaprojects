ceb6e766df7eac654d1ea1075588e3ade707992f
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-13 08:37:10 -0500

    Add files via upload

---
+++ PrimerosProgramas/Final_Key_Word_Example1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package final_key_word_example1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Final_Key_Word_Example1 {
+	public static void main(String[] args) { 
+    final double pi = 3.14;
+    System.out.println(pi);  
+    pi= 2*pi; 
+		
+	}
+}
