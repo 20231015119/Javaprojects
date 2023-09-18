85dbd6bfeb678426bab4e00d229c949c72be4e91
Juan Garay <142996881+20231015119@users.noreply.github.com>
2023-09-18 00:45:48 -0500

    Add files via upload

---
+++ Conditionals/ConditionalExamples.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditional_examples;
+
+/**
+ *
+ * @author david
+ */
+public class ConditionalExamples {
+    
+}
